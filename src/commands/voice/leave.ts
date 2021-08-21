6c2dc3629c6b7a3ad5f92687a9d4a0718943a6f0
be195 <ilya.wicopee@gmail.com>
2021-08-22 00:06:16 +0300

    Changes

---
+++ src/commands/voice/leave.ts
@@ -0,0 +1,25 @@
+import { Context } from 'detritus-client/lib/command';
+
+import { CommandClientExtended } from '../../Application';
+import BaseCommand from '../../BaseCommand';
+import { Voice } from '../../voice';
+
+export default class JoinCommand extends BaseCommand {
+  constructor(commandClient: CommandClientExtended) {
+    super(commandClient, {
+      name: 'leave',
+      aliases: ['l', 'gtfo'],
+    });
+  }
+
+  public async run(ctx: Context) {
+    const res = this.commandClient.application.voices.get(ctx.guild.id);
+    if (!res)
+      return ctx.reply('Not in the voice channel.');
+    if (res.channel !== ctx.member.voiceChannel)
+      return ctx.reply('You are not in the voice channel this bot is currently in.');
+
+    res.kill();
+    return ctx.reply('Gone.');
+  }
+}
