04513e52920f4911449692264c2e65afbaeed3ae
be195 <ilya.wicopee@gmail.com>
2021-08-23 06:37:09 +0200

    Emoji Icons

--- src/constants.ts
+++ src/constants.ts
@@ -13,3 +13,8 @@ export const GOOGLE_COLORS = {
 export const YOUTUBE_APPLICATION_ID = '755600276941176913';
 export const EMBEDDED_APPLICATION = 2;
 export const FILENAME_REGEX = /\.[^/.]+$/;
+
+export const EMOJI_ICONS = {
+  GOOGLE_MIC: '<:googlemicrophone:879221835370213406>',
+  GOOGLE_ASSISTANT: '<:googleassistant:879221886180016159>'
+}
