16ab05a5072eeabe6827f9a1a45aa8acced49088
WicopeeDot <wicobotdiscord@gmail.com>
2021-08-26 07:27:19 +0200

    Fix the same issue again, add a skip command

---
+++ src/commands/voice/skip.ts
@@ -0,0 +1,23 @@
+import { Context } from 'detritus-client/lib/command';
+
+import { CommandClientExtended } from '../../Application';
+import BaseCommand from '../../BaseCommand';
+
+export default class JoinCommand extends BaseCommand {
+  constructor(commandClient: CommandClientExtended) {
+    super(commandClient, {
+      name: 'skip',
+      aliases: ['s'],
+    });
+  }
+
+  public async run(ctx: Context) {
+    const res = this.commandClient.application.voices.get(ctx.guild.id);
+    if (!res)
+      return ctx.reply('Not in the voice channel.');
+    if (res.channel !== ctx.member.voiceChannel)
+      return ctx.reply('You are not in the voice channel this bot is currently in.');
+
+    res.skip();
+  }
+}
