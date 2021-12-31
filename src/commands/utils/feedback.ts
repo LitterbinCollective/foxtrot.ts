a65d9eeb08a5bd1bdfdfb1f1a7afdaa478925427
be195 <ilya.wicopee@gmail.com>
2021-12-31 19:24:25 +0100

    Improve feedback: add anonymous flag, copy attachments

--- src/commands/utils/feedback.ts
+++ src/commands/utils/feedback.ts
@@ -10,21 +10,30 @@ export default class FeedbackCommand extends BaseCommand {
       name: 'feedback',
       aliases: ['fb'],
       consume: true,
-      type: CommandArgumentTypes.STRING
+      type: CommandArgumentTypes.STRING,
+      required: true,
+      args: [
+        {
+          name: 'anonymous',
+          type: CommandArgumentTypes.BOOL,
+          aliases: ['anon', 'a']
+        }
+      ]
     })
   }
 
-  public async run (ctx: Context, { feedback }: ParsedArgs) {
+  public async run (ctx: Context, { feedback, anonymous }: ParsedArgs) {
     let webhook: IConfigFeedbackWebhook
     if (!(webhook = this.commandClient.application.config.feedbackWebhook))
       return ctx.reply('Submitting feedback is temporarily disabled!')
 
     feedback = feedback.replaceAll('@', '@\u200b')
+    ctx.message.attachments.forEach(v => feedback += ' ' + v.url);
 
     this.commandClient.rest.executeWebhook(webhook.id, webhook.token, {
       content: feedback,
-      username: ctx.user.tag + ' (' + ctx.user.id + ')',
-      avatarUrl: ctx.user.avatarUrl
+      username: anonymous ? 'Anonymous#0000' : (ctx.user.tag + ' (' + ctx.user.id + ')'),
+      avatarUrl: anonymous ? null : ctx.user.avatarUrl,
     })
     ctx.reply('Your feedback has been sent to our server, thank you!')
   }
