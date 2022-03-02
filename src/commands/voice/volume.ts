97b47c0f6da21130c0705a4d320698eae681da7c
WicopeeDot <wicobotdiscord@gmail.com>
2022-03-02 02:03:23 +0100

    Add volume command

---
+++ src/commands/voice/volume.ts
@@ -0,0 +1,26 @@
+import { Context, ParsedArgs } from 'detritus-client/lib/command'
+import { CommandArgumentTypes } from 'detritus-client/lib/constants'
+
+import { CommandClientExtended } from '../../Application'
+import BaseCommand from '../../BaseCommand'
+import GoogleAssistantVoiceModule from '../../voice/googleAssistant'
+
+export default class VolumeCommand extends BaseCommand {
+  constructor (commandClient: CommandClientExtended) {
+    super(commandClient, {
+      name: 'volume',
+      aliases: ['v', 'vol'],
+      type: CommandArgumentTypes.FLOAT
+    })
+  }
+
+  public async run (ctx: Context, { volume }: ParsedArgs) {
+    const res = this.commandClient.application.voices.get(ctx.guild.id)
+    if (!res) { return await ctx.reply('Not in the voice channel.') }
+    if (res.channel !== ctx.member.voiceChannel) { return await ctx.reply('You are not in the voice channel this bot is currently in.') }
+    if (!res.initialized)
+      return await ctx.reply('Voice not yet initialized!')
+
+    res.setVolume(volume);
+  }
+}
