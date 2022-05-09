a14b56ec539ee7f0b7bdd7e55e8007fc70fd519d
WicopeeDot <wicobotdiscord@gmail.com>
2022-05-10 00:46:35 +0300

    voice: add ranges for highpass and lowpass effects

--- src/voice/effects/highpass.ts
+++ src/voice/effects/highpass.ts
@@ -6,6 +6,10 @@ export default class HighPassEffect extends BaseEffect {
     frequency: 3700
   }
 
+  public optionsRange = {
+    frequency: [1, 12000],
+  }
+
   public get args (): any[] | boolean {
     if (!this.enabled) return false
     const { frequency } = this.options
