3c82b6ee4583bef9f3bc07cbe5b5b90054e2a97a
WicopeeDot <wicobotdiscord@gmail.com>
2022-05-15 18:36:01 +0300

    voice: a few experimental fixes to recovery

--- src/voice/util.ts
+++ src/voice/util.ts
@@ -7,10 +7,15 @@ export class Skip extends Transform {
   constructor(options: any) {
     super(options);
     this.offset = options.offset;
-    this.on('pipe', (src) => (this.src = src));
+    this.onPipe = this.onPipe.bind(this);
+    this.on('pipe', this.onPipe);
   }
 
-  _transform(chunk: any, _: BufferEncoding, callback: TransformCallback): void {
+  private onPipe(src: any) {
+    this.src = src;
+  }
+
+  public _transform(chunk: any, _: BufferEncoding, callback: TransformCallback): void {
     if (this.offset === 0) this.push(chunk);
     else if (this.offset > chunk.length) this.offset -= chunk.length;
     else {
@@ -18,5 +23,11 @@ export class Skip extends Transform {
       this.offset = 0;
     }
     callback();
+  }
+
+  public kill() {
+    this.off('pipe', this.onPipe);
+    this.unpipe();
+    this.destroy();
   }
 }
