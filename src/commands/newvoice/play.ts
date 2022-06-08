4cad3bff4d85cfeffd78b592e031e8c0ec63053a
be195 <ilya.wicopee@gmail.com>
2022-06-08 19:10:01 +0300

    command: add a newvoice play command

---
+++ src/commands/newvoice/play.ts
@@ -0,0 +1,40 @@
+import { Context, ParsedArgs } from 'detritus-client/lib/command';
+import { CommandArgumentTypes } from 'detritus-client/lib/constants';
+
+import { GMCommandClient } from '../../Application';
+import { BaseCommand } from '../../BaseCommand';
+import NewVoice from '../../voice/new';
+
+export default class NPlayCommand extends BaseCommand {
+  constructor(commandClient: GMCommandClient) {
+    super(commandClient, {
+      name: 'nplay',
+      label: 'url',
+      type: CommandArgumentTypes.STRING,
+      required: true,
+      aliases: ['np'],
+    });
+  }
+
+  public async run(ctx: Context, { url }: ParsedArgs) {
+    if (!ctx.member.voiceChannel)
+      return await ctx.reply('You are not in the voice channel.');
+
+    let voice = this.commandClient.application.newvoices.get(ctx.guild.id);
+    if (!voice) {
+      voice = new NewVoice(
+        this.commandClient.application,
+        ctx.member.voiceChannel,
+        ctx.channel
+      );
+      return voice.queue.push(url, ctx.user);
+    }
+    if (!voice.canExecuteVoiceCommands(ctx.member))
+      return await ctx.reply(
+        'You are not in the voice channel this bot is currently in.'
+      );
+    if (!voice.initialized) return await ctx.reply('Voice not yet initialized!');
+
+    voice.queue.push(url, ctx.user);
+  }
+}
