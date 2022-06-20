307129e056f71737dc9fe3dc9d7ec8cc222b5e6b
be195 <ilya.wicopee@gmail.com>
2022-06-20 18:24:55 +0300

    command: add bitrate command

---
+++ src/commands/newvoice/bitrate.ts
@@ -0,0 +1,28 @@
+import { Context } from 'detritus-client/lib/command';
+import { CommandArgumentTypes } from 'detritus-client/lib/constants';
+
+import { GMCommandClient } from '../../Application';
+import { BaseCommand } from '../../BaseCommand';
+
+export default class SkipCommand extends BaseCommand {
+  constructor(commandClient: GMCommandClient) {
+    super(commandClient, {
+      name: 'nbitrate',
+      aliases: ['nbr', 'nb'],
+      label: 'bitrate',
+      required: true,
+      type: CommandArgumentTypes.NUMBER
+    });
+  }
+
+  public async run(ctx: Context, { bitrate }: { bitrate: number }) {
+    if (!ctx.guild) return;
+    const voice = this.commandClient.application.newvoices.get(ctx.guild.id);
+    if (!voice)
+      return await ctx.reply('Not in the voice channel.');
+    if (!voice.initialized)
+      return await ctx.reply('Voice not yet initialized!');
+
+   voice.bitrate = bitrate;
+  }
+}
