7e1795d78ca540181a04844bb4da7c32a576dd36
be195 <ilya.wicopee@gmail.com>
2025-05-07 04:19:04 +0300

    there was a real reason for why i did this and i overlooked it

--- src/typescript/cluster/utils/audio/ffmpeg.ts
+++ src/typescript/cluster/utils/audio/ffmpeg.ts
@@ -1,8 +1,6 @@
 import { ChildProcess, spawn } from 'child_process';
 import { Transform, Writable } from 'stream';
 
-import { Logger } from '@/utils';
-
 export default class FFMpeg extends Transform {
   public instance!: ChildProcess;
   public offsetTime: number = 0;
@@ -45,7 +43,7 @@ export default class FFMpeg extends Transform {
     );
     (this.instance.stdio[3] as NodeJS.WritableStream).on(
       'error',
-      error => !this.instance.killed && Logger.error(error)
+      error => !this.instance.killed && console.error(error)
     );
     this.instance.on('close', this.ffmpegClose);
   }
